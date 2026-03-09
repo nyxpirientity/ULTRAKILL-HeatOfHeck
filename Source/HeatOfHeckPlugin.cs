06965ed52fa6b6ee077488c8ea99ccdd3cedc6e6
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-03-08 23:14:18 -0700

    Initial Commit

---
+++ Source/HeatOfHeckPlugin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using BepInEx;
+
+namespace Nyxpiri.ULTRAKILL.HeatOfHeck
+{
+    [BepInPlugin("com.nyxpiri.bepinex.plugins.ultrakill.heat-of-heck", "Heat of Heck", "0.0.0.1")]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class HeatOfHeckPlugin : BaseUnityPlugin
+    {
+        protected void Awake()
+        {
+            Log.Initialize(Logger);
+        }
+
+        protected void Start()
+        {
+        }
+
+        protected void Update()
+        {
+
+        }
+
+        protected void LateUpdate()
+        {
+
+        }
+    }
+}
