c1ec9a76b5e20a6455c6ed3e4dc43cd8c50bdf08
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-04-09 02:48:38 -0700

    make explosion speed much more consistent across various sizes

--- Source/HeatOfHeck.cs
+++ Source/HeatOfHeck.cs
@@ -126,7 +126,7 @@ namespace Nyxpiri.ULTRAKILL.HeatOfHeck
             explosion.harmless = explosiveDmgScalar <= 0.0f;
             explosion.lowQuality = false;
             explosion.maxSize = explosiveSize;
-            explosion.speed = explosion.maxSize * 0.03f;
+            explosion.speed = Mathf.Pow(explosion.maxSize * 0.5f, 1.2f) * 0.095f;
             explosion.enemyDamageMultiplier = 1.0f;
             explosion.playerDamageOverride = -1;
             explosion.ignite = true;
